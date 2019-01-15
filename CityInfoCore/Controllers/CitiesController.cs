0283fff83165b1c3195d3d64573e9bca29a1da17
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2019-01-15 11:20:36 +0000

    Setup hardcoded GET api/cities route

---
+++ CityInfoCore/Controllers/CitiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfoCore.Controllers
+{
+    public class CitiesController : Controller
+    {
+        [HttpGet("api/cities")]
+        public IActionResult GetCities()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new { id = 1, Name = "New York City" },
+                new { id = 2, Name = "Antwerp" }
+            });
+        }
+    }
+}
