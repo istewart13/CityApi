76e6cda3584d0582b68c0790331fc876fc96d834
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2019-01-16 14:50:09 +0000

    Add input validation for creating point of interest

--- CityInfoCore/Controllers/PointsOfInterestController.cs
+++ CityInfoCore/Controllers/PointsOfInterestController.cs
@@ -51,6 +51,16 @@ namespace CityInfoCore.Controllers
                 return BadRequest();
             }
 
+            if (pointOfInterest.Description == pointOfInterest.Name)
+            {
+                ModelState.AddModelError("Description", "The provided description should be different from the name.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
 
             if (city == null)
