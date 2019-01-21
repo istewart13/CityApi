2d9bc39e068472cb6afab2594fecde0d01cf3e4d
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2019-01-21 10:16:21 +0000

    Configure app to use CityInfoRepo through DI

--- CityInfoCore/Startup.cs
+++ CityInfoCore/Startup.cs
@@ -38,6 +38,8 @@ namespace CityInfoCore
 #endif
             services.AddDbContext<CityInfoContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<ICityInfoRepository, CityInfoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
