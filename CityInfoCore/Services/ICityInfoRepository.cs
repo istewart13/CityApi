171c5e01f8a9f2e304d392e22ee6e2da96e52b66
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2019-01-21 10:15:58 +0000

    Add interface for repository and implementation

---
+++ CityInfoCore/Services/ICityInfoRepository.cs
@@ -0,0 +1,17 @@
+﻿using CityInfoCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfoCore.Services
+{
+    public interface ICityInfoRepository
+    {
+        IEnumerable<City> GetCities();
+        City GetCity(int cityId, bool includePointsOfInterest);
+        IEnumerable<PointOfInterest> GetPointsOfInterestForCity(int cityId);
+        PointOfInterest GetPointOfInterestForCity(int cityId, int pointOfInterestId);
+   
+    }
+}
